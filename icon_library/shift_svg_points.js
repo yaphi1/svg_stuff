445ab589cec5316f3f270cd634e582d1d79fa76c
yaphi1 <yaphi1@gmail.com>
2016-02-10 17:49:16 -0500

    first commit

---
+++ icon_library/shift_svg_points.js
@@ -0,0 +1,7 @@
+
+
+
+
+'<polygon points="0,28 72,28 60,60 12,60" style="fill:black;"/><polygon points="13.5,66.5 64.5,66.5 84,18 97,18 97,28 88,28 70,74 55,74 17,74" style="stroke:none; fill:black;"/>'.replace(/,[^ "]+/g,function(v){
+		return ','+(Number(v.slice(1))-18);
+	})
